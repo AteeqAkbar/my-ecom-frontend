e13bd31ebfa50195ca425b03f73060d39bfad622
ateeq <attiqdoger3@gmail.com>
2025-02-01 16:36:08 +0500

    fix api url

--- app/services/axiosInstance.js
+++ app/services/axiosInstance.js
@@ -2,10 +2,10 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 // export const baseURL = "http://localhost:1337";
-export const baseURL = "https:api.dico.pk";
+export const baseURL = "https://api.dico.pk";
 export const baseURLfrontend = "https://dico.pk";
 // export const baseURLfrontend = "http://localhost:3000";
-export const baseURLapi = "https:api.dico.pk/api";
+export const baseURLapi = "https://api.dico.pk/api";
 // export const baseURLapi = "http://localhost:1337/api";
 const axiosInstance = axios.create({
   baseURL: baseURLapi, // Replace with your API base URL
